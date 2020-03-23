7b8ee76e99a16616cb610bd373fe25ddd7b6f0f0
Steve Eckles <steve.d.eckles@gmail.com>
2020-03-23 13:12:35 -0400

    Add initial unit tests

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import os
+
+import pytest
+from madness import create_app
+
+@pytest.fixture
+def app():
+    app = create_app({
+        'TESTING': True,
+    })
+
+    with app.app_context():
+        pass
+    yield app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
--- tests/test_madness.py
+++ tests/test_madness.py
@@ -3,3 +3,10 @@ import madness
 def test_import():
     assert madness
 
+def test_config():
+    assert not madness.create_app().testing
+    assert madness.create_app({'TESTING': True}).testing
+
+def test_hello(client):
+    response = client.get('/hello')
+    assert response.data == b'Hello, World!'
